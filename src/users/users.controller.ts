ce49065e83e855a2dcfc778f4305fc0658b999b2
developer <ch9716@naver.com>
2023-10-23 21:16:10 +0900

    nestjs cli 로 users 리소스 생성

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAllUser(@Query('email') email: string) {
+    return await this.usersService.find(email);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('유저를 찾을 수 없습니다');
+    return user;
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create() {
+    return 'This action adds a new user';
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  findOne(id: number) {
+    if (!id) return null;
+    return this.repo.findOne({ where: { id } });
+  }
+
+  update() {}
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
